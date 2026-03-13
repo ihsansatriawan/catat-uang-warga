ecf17ea6227f92a1019b8536bed6be70270b50c3
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2026-03-14 05:51:25 +0700

    feat: add /broadcast route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import LeaderboardView from './components/LeaderboardView'
+import BroadcastView from './components/BroadcastView'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/leaderboard" element={<LeaderboardView />} />
+      <Route path="/broadcast" element={<BroadcastView />} />
     </Routes>
   )
 }
