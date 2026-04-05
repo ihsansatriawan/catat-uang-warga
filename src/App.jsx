a5c6d599594c189e74a69abbfa967d071f1fd0d4
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2026-04-05 16:32:00 +0700

    feat: add /warga/:blok/:nomorRumah route with WargaPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
+import WargaPage from './components/WargaPage'
 import LeaderboardView from './components/LeaderboardView'
 import BroadcastView from './components/BroadcastView'
 import ExpensesView from './components/ExpensesView'
@@ -8,6 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/warga/:blok/:nomorRumah" element={<WargaPage />} />
       <Route path="/leaderboard" element={<LeaderboardView />} />
       <Route path="/broadcast" element={<BroadcastView />} />
       <Route path="/pengeluaran" element={<ExpensesView />} />
